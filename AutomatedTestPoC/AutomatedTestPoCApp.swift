4ea32e6d9b0791e900dfc21abc88193f65cd7a5e
Bright Edem Gawu <54558350+brightedemgawu@users.noreply.github.com>
2025-04-02 11:38:14 +0000

    add initial project setup

---
+++ AutomatedTestPoC/AutomatedTestPoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomatedTestPoCApp.swift
+//  AutomatedTestPoC
+//
+//  Created by AMALITECH-PC-105889 on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AutomatedTestPoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutomatedTestPoC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AutomatedTestPoC
+//
+//  Created by AMALITECH-PC-105889 on 4/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
